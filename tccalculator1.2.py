4cec83a50a552e11f5ba7c35a4875cd682054544
Vic Herrera <126992509+vhhr7@users.noreply.github.com>
2024-07-17 18:30:56 -0500

    Update tccalculator1.2.py
    El usuario va a ver el formato arreglado cuando lo ingrese mal

--- tccalculator1.2.py
+++ tccalculator1.2.py
@@ -83,8 +83,10 @@ def main():
 
     st.write("## Timecode to Seconds Conversion")
     timecode_input = st.text_input("Enter Timecode or as a continuous string of numbers", value="00:00:00:00")
+    formatted_timecode_input = parse_timecode_input(timecode_input)
     if st.button("Convert to Seconds"):
         seconds = timecode_to_seconds(timecode_input, frame_rate)
+        st.write(f"Formatted Timecode: {formatted_timecode_input}")
         st.write(f"Total seconds: {seconds}")
 
     st.write("## Seconds to Timecode Conversion")
@@ -97,16 +99,22 @@ def main():
     operation = st.selectbox("Select Operation", ["Add", "Subtract"])
     timecode1 = st.text_input("Timecode 1 format or as a continuous string of numbers", value="00:00:00:00")
     timecode2 = st.text_input("Timecode 2 format or as a continuous string of numbers", value="00:00:00:00")
+    formatted_timecode1 = parse_timecode_input(timecode1)
+    formatted_timecode2 = parse_timecode_input(timecode2)
     
     if operation == "Add":
         if st.button("Perform Operation"):
             total_seconds = timecode_to_seconds(timecode1, frame_rate) + timecode_to_seconds(timecode2, frame_rate)
             result_timecode = seconds_to_timecode(total_seconds, frame_rate)
+            st.write(f"Formatted Timecode 1: {formatted_timecode1}")
+            st.write(f"Formatted Timecode 2: {formatted_timecode2}")
             st.write(f"Result: {result_timecode}")
     elif operation == "Subtract":
         if st.button("Perform Operation"):
             total_seconds = timecode_to_seconds(timecode1, frame_rate) - timecode_to_seconds(timecode2, frame_rate)
             result_timecode = seconds_to_timecode(total_seconds, frame_rate)
+            st.write(f"Formatted Timecode 1: {formatted_timecode1}")
+            st.write(f"Formatted Timecode 2: {formatted_timecode2}")
             st.write(f"Result: {result_timecode}")
 
     display_footer()
